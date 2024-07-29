237f6548c39bc50081b9c8d83124a179f9e563c2
natalagoncaruk <honcharukna@gmail.com>
2024-07-29 19:43:31 +0200

    form sign in/up + work with tailwind styles

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from "./assets/Components/Login/Login.jsx";
+
+function App() {
+
+  return (
+      <div>
+        <Login />
+      </div>
+
+  )
+}
+
+export default App
